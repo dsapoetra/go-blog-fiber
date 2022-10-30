f7cc62df8aef8f5010341b7c69e0ae78d4fdd99a
dimassaputra <dimas.angga@wartek.belajar.id>
2022-10-30 08:28:41 +0700

    fix is deleted author

--- app/model/repo/author.go
+++ app/model/repo/author.go
@@ -12,5 +12,5 @@ type Author struct {
 	FullName  string    `db:"full_name" json:"full_name" validate:"required"`
 	UserName  string    `db:"username" json:"username" validate:"required"`
 	Password  string    `db:"password" json:"password" validate:"required"`
-	IsDeleted bool      `db:"is_deleted" json:"password" validate:"required"`
+	IsDeleted bool      `db:"is_deleted" json:"is_deleted" validate:"required"`
 }
